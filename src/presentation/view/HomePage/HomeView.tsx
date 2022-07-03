e9c6e4b3888a4abb45e175f53b67a3d581588424
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-07-03 19:09:33 -0300

    Remove checkbox não usada
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- src/presentation/view/HomePage/HomeView.tsx
+++ src/presentation/view/HomePage/HomeView.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, Grid, Stack, Switch, TextareaAutosize, TextField, Typography } from "@mui/material"
+import { Box, Button, FormControl, Grid, Stack, TextareaAutosize, TextField, Typography } from "@mui/material"
 import HomeViewModel from "./HomeViewModel"
 
 function useViewModel() {
